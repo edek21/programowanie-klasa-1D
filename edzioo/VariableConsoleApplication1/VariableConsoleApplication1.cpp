f7c0c1c32bd797c697dfe48db21c839979befbeb
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-18 08:18:00 +0200

    lo

---
+++ edzioo/VariableConsoleApplication1/VariableConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+/*
+Algorytm - zbiór instrukcji realizuj¹cy postawiony przed nim zadanie
+sposób zapisu:
+* w punktach
+* opis s³owny
+* jêzyk programowania lub pseudokodd
+* zbiór rysunków
+* schemat blokowy
+
+*/

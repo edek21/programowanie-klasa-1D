2334d31362ddd3da79646e2cd1079d4fc7d9c8f9
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-12-20 09:38:11 +0100

    20.12.2023

--- edzioo/InstructionConsoleApplication/InstructionConsoleApplication.cpp
+++ edzioo/InstructionConsoleApplication/InstructionConsoleApplication.cpp
@@ -18,9 +18,66 @@ void task1()
 
 }
 
+void task2()
+{
+	int number;
+	std::cout << "Podaj liczbe :\n";
+	std::cin >> number;
+
+	if (number >= 1)
+	{
+		if (number < 10)
+		{
+			std::cout << "Liczba jest w zakresie\n";
+		}
+		else
+		{
+			std::cout << "liczba nie jest w zakresie\n";
+		}
+	}
+	else
+	{
+		std::cout << "liczba nie jest w zakresie\n";
+	}
+}
+//Napisz program, który wczyta jedn¹ wartoœæ i wyœwietli najwiêksz¹ z nich.
+void task3()
+{
+	int firstNumber;
+	std::cout << "Podaj liczbê\n";
+	std::cin >> firstNumber;
+
+	std::cout << "Najwiêksza wartoœæ to:" << firstNumber << "\n";
+
+
+}
+//
+void task4()
+{
+	int firstNumber, secondNumber, thirdNumber; 
+	std::cout << "Podaj liczbe\n";
+	std::cin >> firstNumber;
+	std::cout << "Podaj liczbe\n";
+	std::cin >> secondNumber;
+	std::cout << "Podaj liczbe\n";
+	std::cin >> thirdNumber;
+
+	if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+		std::cout << "Najwiêksza wartoœæ to" << secondNumber << "\n";
+	else
+	{
+		if (secondNumber > firstNumber)
+			std::cout << " Najwiêksza liczba to" << secondNumber << "\n";
+		else
+			std::cout << "Najwiêksza wartoœæ to " << firstNumber << "\n"
+	}
+
+}
 int main()
 {
-	task1();
+	//task1();
+	task2();
+	task3();
 }
 
 

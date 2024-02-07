e512847407c3f20e0062c1dd6d0d50491ceb2d83
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-02-07 10:30:25 +0100

    07.02

--- edzioo/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ edzioo/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -10,11 +10,34 @@ void task2()
 	std::cout << "Liczba:" << randomNumber;
 
 }
+void task3()
+{
+	int upperRange;
 
+	std::cout << "Podaj górnyh zakres:";
+	std::cin >> upperRange;
+
+
+	std::cout << "0, ";
+	if (upperRange >= 1)
+	{
+		std::cout << "1, ";
+		if (upperRange >= 2)
+		{
+			std::cout << "2, ";
+			if (upperRange >= 3)
+			{
+				std::cout << "3, ";
+
+			}
+		}
+	}
+}
 
 int main()
 {
-	task2();
+	//task2();
+	task3();
 }
 
 

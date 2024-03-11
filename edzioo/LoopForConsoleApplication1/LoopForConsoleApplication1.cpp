d824bf383198a63b09de3b843d3ffe546c43a32b
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-03-11 08:32:21 +0100

    11.03

--- edzioo/LoopForConsoleApplication1/LoopForConsoleApplication1.cpp
+++ edzioo/LoopForConsoleApplication1/LoopForConsoleApplication1.cpp
@@ -1,5 +1,15 @@
-﻿
-
+﻿/*
+* Program obliczający sumę liczb od 1 do 100
+* Program sprawdzający czy podana liczba jest liczbą doskonałą (czyli taką, której suma dzielników (z wyłączeniem samej siebie) jest równa danej liczbie, np. 6 jest liczbą doskonałą, ponieważ 1 + 2 + 3 = 6).
+* Program wyświetlający na ekranie kwadraty liczb od 1 do 10 (np. 1, 4, 9, 16 itd.)
+* Program obliczający n!.
+* Program wyświetlający na ekranie silnie z liczb od 1 do 10 (np. 1!, 2!, 3!, 4! itd.)
+* Program wyświetlający na ekranie tabliczkę mnożenia od 1 do 9 (np. 1x1=1, 1x2=2, 1x3=3 itd.).
+* Program wyświetlający na ekranie wszystkie liczby podzielne przez 3 z zakresu od 1 do 100
+* Program obliczający sumę kwadratów liczb od 1 do 10
+* Program, który wczyta podstawę oraz wykładnik oraz wyliczy wynik.
+* Program wyświetlający na ekranie ciąg Fibonacciego do 20 elementu (ciąg Fibonacciego to ciąg gdzie każdy element jest sumą dwóch poprzednich, np. 0, 1, 1, 2, 3, 5, 8, 13 itd.)
+*/
 #include <iostream>
 //Napisz program, który wyœwietli "Hello World" tyle razy ile chce u¿ytkownik
 void task1()
@@ -27,9 +37,47 @@ void task2()
 	}
 }
 
+
+
+	//Namaluj równoleglobok z "X". Wysokosc i szeokosc wczytaj z klawiatury.
+void task3()
+{
+	int width, height;
+
+	do
+	{
+		std::cout << "Podaj szerokoœæ: \n";
+		std::cin >> width;
+	} while (width < 0);
+
+	do
+	{
+		std::cout << "Podaj wysokoœæ: \n";
+		std::cin >> height;
+	} while (height < 0);
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			std::cout << "X";
+		}
+		std::cout << "\n";
+	}
+
+}
+//Program obliczający sumę liczb od 1 do 100
+void task4()
+{
+
+
+
+}
 int main()
 {
 	//task1();
-	task2();
+	//task2();
+	//task3();
+	task4();
 }
 

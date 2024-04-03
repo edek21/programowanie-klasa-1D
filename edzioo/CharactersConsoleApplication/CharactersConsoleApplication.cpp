a7fbef495bb2652daf5ae45c2b26e4d846b28ded
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-03 10:41:33 +0200

    03.04

--- edzioo/CharactersConsoleApplication/CharactersConsoleApplication.cpp
+++ edzioo/CharactersConsoleApplication/CharactersConsoleApplication.cpp
@@ -15,11 +15,60 @@ void task1()
 	characterfromuser = 'a';
 	characterfromuser = 'a' + 1; 
 }
+void task2()
+{
+	char characterFromUser;
+	std::cout << "Podaj jeden znak:\n";
+	std::cin >> characterFromUser; 
 
+	if (characterFromUser == 'a'
+		|| characterFromUser == 'b'
+		|| characterFromUser == 'c'
+		//...
+		|| characterFromUser == 'z')
+	{
+		std::cout << "Poda³eœ ma³a litere alfabetu \n";
+	}
+	else
+	{
+		std::cout << "Poda³eœ inny zmnak \n";
+	}
+}
+void task3()
+{
+	std::string userName;
+	std::cout << "Podaj swoje imie \n";
+	std::cin >> userName;
+
+	std::cout << "Witaj" << userName << "tutaj.\n";
+}
+void task4()
+{
+	std::string password;
+	std::cout << "Podaj has³o\n";
+	std::cin >> password;
+
+	if (password == "abc123")
+	{
+		std::cout << "Has³o poprawne\n";
+	}
+	else
+		std::cout << "Has³o niepoprawne\n";
+}
+void task5()
+{
+
+
+
+}
 
 int main()
 {
-	task1();
+	//task1();
+	//task2();
+	//task3();
+	//task4();
+	task5();
 }
 
 

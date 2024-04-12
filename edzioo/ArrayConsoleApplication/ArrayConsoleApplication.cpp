95024cc51e135b6d6b870b0dbcc4cae29c7796cf
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-04-12 10:38:51 +0200

    12.04

--- edzioo/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ edzioo/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -1,20 +1,28 @@
-// ArrayConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
+//Napisz program, który wczyta 5 liczb a nast?pnie wy?wietli je w odwrotnej kolejno?ci.
+void task1()
+{
+    const unsigned int ARRAY_NUMBER_SIZE = 5;
+
+    int numbers[ARRAY_NUMBER_SIZE];
+    for (int i = 0; i < ARRAY_NUMBER_SIZE; i++)
+    {
+        std::cout << "Podaj " << (i + 1) << " liczb?\n";
+        std::cin >> numbers[i];
+    }
+
+    for (int i = ARRAY_NUMBER_SIZE - 1; i >= 0; i--)
+    {
+        std::cout << numbers[i] << ", ";
+    }
+    std::cout << "\n";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    task1();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

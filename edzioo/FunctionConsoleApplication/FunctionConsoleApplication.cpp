c9eca11ecdb47afd1245ae6524685e747a766ab2
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-05-17 14:23:22 +0200

    17.05

---
+++ edzioo/FunctionConsoleApplication/FunctionConsoleApplication.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+/*
+Funkcja - zbiór instrukcji. Definicja/deklaracja funkcji sk³ada siê z dwóch czêœci:
+* nagówek - sposób u¿ycia funkcji
+	- typ zwracany
+	- nazwa (poprawny identyfikator)
+	- parametrów w nawiasie (nieobowi¹z kowe)
+* cia³o - zbiór instrukcji
+
+typZwracany nazwaFuncji(parametry)
+{
+	//cia³o funkcji
+}
+*/
+
+void showHello()
+{
+	std::cout << "Hello World";
+}
+int main()
+{    
+	showHello();
+
+}
+
+

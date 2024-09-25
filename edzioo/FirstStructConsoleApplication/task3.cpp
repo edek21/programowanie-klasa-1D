b235e9ed49b18474a3132b9e9a59e3074d740501
q <q>
2024-09-25 09:00:37 +0200

    25.09.2024

---
+++ edzioo/FirstStructConsoleApplication/task3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+struct bankAccount
+{
+	double balance; // stan konta
+	std::string owner; //w³aœciciel
+	std::string currency; // waluta
+};
+
+void accountInformation(bankAccount& account)
+{
+	std::cout << "Informacje o koncie bankowym.\n";
+	std::cout << "W³aœciciel: " << account.owner << "\n";
+	std::cout << "Saldo: " << account.balance << " " << account.currency << "\n\n";
+}
+
+void depositToAccount(bankAccount& account, double amount)
+{
+	account.balance = account.balance + amount;
+}
+
+void task3()
+{
+	bankAccount firstAccount;
+	firstAccount.balance = 10000;
+	firstAccount.owner = "Jan Kowalski";
+	firstAccount.currency = "z³";
+
+	accountInformation(firstAccount);
+	depositToAccount(firstAccount, 500);
+	accountInformation(firstAccount);
+
+
+	bankAccount secondAccount;
+	secondAccount.balance = 15000;
+	secondAccount.owner = "Ewa Kowalska";
+	secondAccount.currency = "z³";
+
+	accountInformation(secondAccount);
+}
